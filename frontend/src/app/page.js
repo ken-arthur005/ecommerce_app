31db9ffdb8c7a5b2a82ee4e4358382d9987cc39d
ken-arthur005 <careyckenarthur@gmail.com>
2025-03-25 16:47:29 +0000

    new changes to the folder structure

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,15 +1,15 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    
+      <main>
         <h1>Hello, World!</h1>
         <h2>My name is CAREY, and i'm here to take over!!!</h2>
         
       </main>
      
       
-    </div>
+   
   );
 }
